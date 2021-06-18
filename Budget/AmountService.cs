84d8f307b4360ee18878f5181a123f932276845d
Damian Tkocz <44502850+Branwie@users.noreply.github.com>
2021-06-18 16:26:20 +0200

    Add files via upload

---
+++ Budget/AmountService.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget
+{
+    class AmountService
+    {
+        public List<Amount> Amounts { get; set; }
+
+        public AmountService()
+        {
+            Amounts = new List<Amount>();
+        }
+
+        public ConsoleKeyInfo AddNewAmountView(MenuActionService actionService)
+        {
+            var addNewAmountMenu = actionService.GetMenuActionsByMenuName("AddNewAmountMenu");
+            Console.WriteLine("\r\nPlease select type:");
+            for (int i = 0; i < addNewAmountMenu.Count; i++)
+            {
+                Console.WriteLine($"{addNewAmountMenu[i].Id }. { addNewAmountMenu[i].Name }");
+            }
+
+            var operation = Console.ReadKey();
+            return operation;
+        }
+
+        public int AddNewAmount(char amountType)
+        {
+            int amountTypeId;
+            Int32.TryParse(amountType.ToString(), out amountTypeId);
+            Amount amount = new Amount();
+            amount.TypeId = amountTypeId;
+            Console.WriteLine("\r\nPlease enter id for new amount:");
+            var id = Console.ReadLine();
+            int amountId;
+            Int32.TryParse(id, out amountId);
+            Console.WriteLine("\r\nPlease enter name for new amount:");
+            var name = Console.ReadLine();
+            Console.WriteLine("\r\nPlease enter value for new amount: ");
+            var value = Console.ReadLine();
+            int amountValue;
+            Int32.TryParse(value, out amountValue);
+
+
+            amount.Id = amountId;
+            amount.Name = name;
+            amount.Value = amountValue;
+            
+
+
+            Amounts.Add(amount);
+            return amountId;
+        }
+
+        public int RemoveAmountView()
+        {
+            Console.WriteLine("\r\nPlease enter id for amount to remove:");
+            var amountId = Console.ReadKey();
+            int id;
+            Int32.TryParse(amountId.KeyChar.ToString(), out id);
+
+            return id;
+        }
+
+
+        public void RemoveAmount(int removeId)
+        {
+            Amount amountToRemove = new Amount();
+            foreach(var amount in Amounts)
+            {
+                if(amount.Id == removeId)
+                {
+                    amountToRemove = amount;
+                    Amounts.Remove(amountToRemove);
+                    break;
+                }
+                
+            }
+        }
+
+        public int AmountDetailSelectionView()
+        {
+            Console.WriteLine("\r\nPlease enter id for amount to show:");
+            var amountId = Console.ReadKey();
+            int id;
+            Int32.TryParse(amountId.KeyChar.ToString(), out id);
+
+            return id;
+        }
+
+        public void AmountDetailView(int detailId)
+        {
+            Amount amountToShow = new Amount();
+            foreach (var amount in Amounts)
+            {
+                if (amount.Id == detailId)
+                {
+                    amountToShow = amount;
+                    break;
+                }
+            }
+            Console.WriteLine($"---------------------------");
+            Console.WriteLine($"Amount id: {amountToShow.Id}");
+            Console.WriteLine($"Amount name: {amountToShow.Name}");
+            Console.WriteLine($"Amount Type Id: {amountToShow.TypeId}");
+            Console.WriteLine($"Amount Value: {amountToShow.Value}");
+            Console.WriteLine($"---------------------------");
+        }
+
+        public int AmountTypeSelectionView()
+        {
+            Console.WriteLine("\r\nPlease enter 1 to show incomes or 2 to show expenses:");
+            var amountId = Console.ReadKey();
+            int id;
+            Int32.TryParse(amountId.KeyChar.ToString(), out id);
+
+            return id;
+        }
+
+        public void AmountByTypeId(int typeId)
+        {
+            List<Amount> toShow = new List<Amount>();
+            foreach(var amount in Amounts)
+            {
+
+                if (amount.TypeId == typeId)
+                {
+                    Console.WriteLine($"---------------------------");
+                    Console.WriteLine("Amount information:");
+                    Console.WriteLine($"Amount id: {amount.Id}");
+                    Console.WriteLine($"Amount name: {amount.Name}");
+                    Console.WriteLine($"Amount Value: {amount.Value}");
+                    Console.WriteLine($"---------------------------");
+                }
+            }
+        }
+
+        public void Calculation()
+        {
+            int currentBudget = 0;
+            int income = 0;
+            int expense = 0;
+            List<Amount> toCalculate = new List<Amount>();
+            foreach(var amount in Amounts)
+            {
+                if(amount.TypeId == 1)
+                {
+                    income = income + amount.Value;
+                }
+
+                else if (amount.TypeId == 2)
+                {
+                    expense = expense + amount.Value;
+                }
+
+                currentBudget = income - expense;
+
+
+            }
+            Console.WriteLine($"---------------------------");
+            Console.WriteLine($"Your current budget: {currentBudget}");
+            Console.WriteLine($"---------------------------");
+        }
+    }
+}
---
+++ Budget/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Budget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuActionService actionService = new MenuActionService();
+            AmountService amountService = new AmountService();
+            actionService = Initialize(actionService);
+
+            Console.WriteLine("Welcome to Home budget app!!!");
+            while (true)
+            {
+
+           
+                Console.WriteLine("\r\nPlease let me know what you want to do: ");
+                var mainMenu = actionService.GetMenuActionsByMenuName("Main");
+                for (int i = 0;i < mainMenu.Count; i++)
+                {
+                    Console.WriteLine($"{mainMenu[i].Id }. { mainMenu[i].Name }");
+                }
+
+                var operation = Console.ReadKey();
+                    switch (operation.KeyChar)
+                    {
+                        case '1':
+                            var keyinfo = amountService.AddNewAmountView(actionService);
+                            var id = amountService.AddNewAmount(keyinfo.KeyChar);
+                            break;
+                        case '2':
+                            var removeId = amountService.RemoveAmountView();
+                            amountService.RemoveAmount(removeId);
+                            break;
+                        case '3':
+                            var detailId = amountService.AmountDetailSelectionView();
+                            amountService.AmountDetailView(detailId);
+                            break;
+                        case '4':
+                            var typeId = amountService.AmountTypeSelectionView();
+                            amountService.AmountByTypeId(typeId);
+                            break;
+                        case '5':
+                            amountService.Calculation();
+                            break;
+                        default:
+                            Console.WriteLine("Action you entered does not exist");
+                            break;
+                        
+                    }
+            }
+
+        }
+
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.AddNewAction(1, "Add Amount", "Main");
+            actionService.AddNewAction(2, "Remove Amount", "Main");
+            actionService.AddNewAction(3, "Show details", "Main");
+            actionService.AddNewAction(4, "List of Amounts", "Main");
+            actionService.AddNewAction(5, "Calculate current budget", "Main");
+            
+            actionService.AddNewAction(1, "Income", "AddNewAmountMenu");
+            actionService.AddNewAction(2, "Expense", "AddNewAmountMenu");
+            
+
+            return actionService;
+        }
+
+
+    }
+}
